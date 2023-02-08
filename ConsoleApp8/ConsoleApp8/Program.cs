91e8fff38376339f18aceb0feaa9518f3ad5a34b
TofDowe <68544755+TofDoweHub@users.noreply.github.com>
2023-02-08 20:26:51 +0500

    Add files via upload

---
+++ ConsoleApp8/ConsoleApp8/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string userName = Console.ReadLine();
+			string userInputPassword = Console.ReadLine();
+			string userInputPasswordConfirmation = Console.ReadLine();
+
+			bool ableToLogin = userInputPassword == userInputPasswordConfirmation;
+			// Переменная сделана с учётом того, что эта программа типо будет потом развиваться, и её функционал понадобится ещё раз
+
+			if (ableToLogin)
+			{
+				Console.WriteLine($"Добро пожаловать, {userName}!");
+				Console.WriteLine("*Функционал приложения открыт*");
+			}
+			else
+			{
+				Console.WriteLine("Пароли не совпадают! Попробуйте ещё раз.");
+				Console.WriteLine("*Вылезла ошибка, функционал приложения не открыт*");
+			}
+
+			Console.ReadLine();
+
+		}
+	}
+}
